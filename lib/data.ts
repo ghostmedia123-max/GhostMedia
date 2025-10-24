09c1f9173580986abae2ff5a859f5197f1cbe306
MacBookAir <macbookair@MacBookAirs-MacBook-Air.local>
2025-10-24 12:23:13 -0400

    fix

--- lib/data.ts
+++ lib/data.ts
@@ -133,7 +133,7 @@ export async function getServicesData() {
   try {
     // Fetches services selected for the homepage
     const query = groq`*[_type == "servicesManagement"][0]{
-      "services": homepageServices[]->{...}
+      "services": homepageServices[]{title, serviceIcon}
     }`;
     const data = await client.fetch<{services: Service[]}>(query);
     return data?.services || []; // Return an empty array if no services are found
